9a3c536d78c997fe64cdc5d93db76c53e2d1bcf8
djarce <debora.arce@live.com.ar>
2020-07-21 10:18:52 -0300

    applying Number()
    Changing parseInt() for Number()

--- clase8/adivina/adivinaElNumero.js
+++ clase8/adivina/adivinaElNumero.js
@@ -1,7 +1,7 @@
 //Definir un número secreto.
 let secretNumber = 16;
 //Pedirle al usuario que adivine el número.
-let adivina = parseInt(prompt("Adivina el número secreto. Ingresa un número del 0 al 100."));
+let adivina = Number(prompt("Adivina el número secreto. Ingresa un número del 0 al 100."));
 /*Dejarle saber al usuario si adivinó o no el número. Decirle si el número secreto es más alto
  o más bajo que su número. */
 if (adivina === secretNumber) {
